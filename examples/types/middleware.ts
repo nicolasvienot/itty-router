33d1ef644a9d14dfa43f9b1ddb1df6350e8f306a
Kevin Whitley <krwhitley@gmail.com>
2024-04-02 10:29:47 -0500

    expanded middleware example to include second middleware

--- examples/types/middleware.ts
+++ examples/types/middleware.ts
@@ -5,12 +5,21 @@ type UserRequest = {
   user: string
 } & IRequestStrict
 
+type PetRequest = {
+  pet: string
+} & IRequestStrict
+
 // we define a *strict* router for this demo
 const router = IttyRouter<IRequestStrict>()
 
 // middleware with explicit request-type (generic)
 const withUser: RequestHandler<UserRequest> = (request) => {
   request.user = 'Kevin'
+}
+
+// middleware with explicit request-type (generic)
+const withPet = (request: PetRequest) => {
+  request.pet = 'Halsey'
 }
 
 router
@@ -20,8 +29,9 @@ router
   })
 
   // then we add the middleware defined above, allowing the handler chain to inherit the request type
-  .all('*', withUser, (request) => {
-    request.user = 'Kevin'
+  .all<UserRequest & PetRequest>('*', withUser, withPet, (request) => {
+    request.user = 'Kevin'  // should work
+    request.pet = 'Halsey' // should work
   })
 
   // request will be back to IRequestStrict here, thus no user property
