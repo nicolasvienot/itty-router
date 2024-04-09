3c688e2250c9e61b0c6125c6b6aaf4bde94eb369
Kevin Whitley <krwhitley@gmail.com>
2024-04-08 22:50:31 -0500

    released v5.0.11 - code-golfing

--- src/withParams.ts
+++ src/withParams.ts
@@ -1,9 +1,10 @@
 import { IRequest } from './types'
 
 export const withParams = (request: IRequest): void => {
-  request.proxy = new Proxy(request.proxy || request, {
-    get: (obj, prop) => obj[prop] !== undefined
-                    ? obj[prop]?.bind?.(request) || obj[prop]
-                    : obj?.params?.[prop]
+  request.proxy = new Proxy(request.proxy ?? request, {
+    get: (obj, prop) =>
+            obj[prop]?.bind?.(request)  // if prop exists (as function), return the function, bound to the original request
+            ?? obj[prop]                // if prop exists, return it
+            ?? obj?.params?.[prop]      // if no prop exists, try the params object
   })
 }
