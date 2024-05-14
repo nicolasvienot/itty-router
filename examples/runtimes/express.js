f4d79f0df30e06aad03a2d82bd4988802e177517
Elya Livshitz <elya.livshitz@gmail.com>
2024-05-14 20:31:27 +0300

    fix express.js example (#247)
    Should be using `createServerAdapter` adapter to convert to express-compatible handler.
    Based on this issue conversation: https://github.com/kwhitley/itty-router/issues/246#issuecomment-2110506175

--- examples/runtimes/express.js
+++ examples/runtimes/express.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import { Router, error, json } from 'itty-router'
 import 'isomorphic-fetch'
+import { createServerAdapter } from '@whatwg-node/server'
 
 const app = express()
 
 const router = Router()
 
-router.get('/', () => 'Success!').all('*', () => error(404))
+router.get('/', () => 'Success!')
+router.all('*', () => error(404))
 
-const handle = (request) => router.handle(request).then(json).catch(error)
+const handle = (request) => router.fetch(request).then(json).catch(error)
 
-app.use(handle)
+const ittyServer = createServerAdapter(handle)
+app.use(ittyServer.handle)
 
 app.listen(3001)
 console.log('listening at https://localhost:3001')
